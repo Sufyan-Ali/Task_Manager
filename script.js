ea2d0bd3b7293c86d8a75570f4fdad92bf0596ca
Sufyan Ali <sufyanali3157426@gmail.com>
2026-06-21 14:25:11 +0200

    Add task and Delete Task Implemented

---
+++ script.js
@@ -0,0 +1,37 @@
+let tasks = []
+let searchedTasks= []
+let nextID = 1
+
+const input = document.getElementById("input")
+const button = document.getElementById("addTaskButton")
+button.addEventListener("click", addTask)
+const list = document.getElementById("list")
+
+function addTask(){
+    if (input.value == "") {
+        return
+    }
+    tasks.push({id: nextID ,task: input.value})
+    nextID++
+    input.value = ""
+    reRenderUI()
+}
+list.addEventListener("click", function(event){
+    const clickedTag = event.target
+    if(clickedTag.tagName.toLowerCase() == "button"){
+        deleteTask(clickedTag.dataset.id)
+    }
+    
+})
+
+function deleteTask(id){
+    tasks = tasks.filter(task => task.id != id)
+    reRenderUI()
+}
+function reRenderUI(showTasks){
+    list.innerHTML = ""
+    for (let l = 0; l < tasks.length; l++) {
+        list.innerHTML += "<div><li>"+tasks[l].task  +"<button id=deleteTaskButton data-id="+ tasks[l].id +">Delete</button></li></div>"
+    }
+}
+console.log("Javascript Connected")
